327fd5f038e77c2641aad178cf36294850b6e72b
Oksu Park <pos91011@gmail.com>
2023-01-04 23:02:08 +0900

    solve Morse1 question

---
+++ CSharp/code_exercies/Programmers/Morse1.cs
@@ -0,0 +1,25 @@
+//Morse.cs
+
+using System;
+
+public class Solution {
+    public string solution(string letter) {
+        
+        string[] morses = letter.Split(" ");
+        return DecodeMorseCode(morses);
+    }
+    
+    public string DecodeMorseCode(string[] morses) {
+        string decodeValue = "";
+        string[] morseTable = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        foreach(string aMorse in morses) {
+            for(int i = 0; i < morseTable.Length; i++) {
+                if(morseTable[i].Equals(aMorse)) {
+                    decodeValue += (char)('a' + i);
+                    break;
+                }
+            }
+        }
+        return decodeValue;
+    }
+}
