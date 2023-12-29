d2512cae7feb8e62b5d51df3691742209bb5b948
ospark <pos91011@gmail.com>
2023-12-29 13:31:02 +0900

    change the solution of EscapeBead2

--- Java/code_exercies/Baekjoon/src/gold/EscapeBead2.java
+++ Java/code_exercies/Baekjoon/src/gold/EscapeBead2.java
@@ -3,7 +3,8 @@ package gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
@@ -13,98 +14,112 @@ import java.util.StringTokenizer;
 public class EscapeBead2 {
     static char[][] map;
     static int[][] moving = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
+    static int answer = 11;
+    static int N;
+    static int M;
+    static boolean[][][][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        final int N = Integer.parseInt(st.nextToken());
-        final int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         map = new char[N][M];
-        PriorityQueue<Node> queue = new PriorityQueue<>();
+        Queue<PositionAndCnt> queue = new LinkedList<>();
+        int xRed = 0, yRed = 0, xBlue = 0, yBlue = 0;
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
             for (int j = 0; j < M; j++) {
                 map[i][j] = line.charAt(j);
                 switch (map[i][j]) {
-                    case 'R' -> queue.add(new Node('R', i, j, 0));
-                    case 'B' -> queue.add(new Node('B', i, j, 0));
-                    default -> { /*do nothing */ }
-                }
-            }
-        }
-        //bfs
-        int answer = 10;
-        boolean isExitRed = false;
-        boolean isExitBlue = false;
-
-        while (!queue.isEmpty()) {
-            Node poll = queue.poll();
-            System.out.println(poll.color + " " + poll.x + " " +poll.y);
-            int count = poll.count;
-            if (count == 10 || count >= answer) continue;
-            for (int[] step : moving) {
-                int nextX = poll.x + step[0];
-                int nextY = poll.y + step[1];
-                //case which can move
-                if((map[nextX][nextY] == 'R' || map[nextX][nextY] == 'B')
-                        && map[nextX + step[0]][nextY + step[1]] == '.') {
-                    System.out.println("hello");
-                    queue.add(new Node(poll.color, poll.x, poll.y, count));
-                    continue;
-                }
-                while (map[nextX][nextY] == '.') {
-                    nextX += step[0];
-                    nextY += step[1];
-                }
-                //case which is same area
-                if(poll.x == nextX - step[0] && poll.y == nextY - step[1]) continue;
-                switch (map[nextX][nextY]) {
-                    case 'O':
-                        if (poll.color == 'B') {
-                            isExitBlue = true;
-                        } else {
-                            isExitRed = true;
-                            answer = count + 1;
-                        }
-                        map[poll.x][poll.y] = '.';
-                        break;
                     case 'R':
+                        xRed = i;
+                        yRed = j;
+                        break;
                     case 'B':
-                    case '#':
-                        map[poll.x][poll.y] = '.';
-                        map[nextX - step[0]][nextY - step[1]] = poll.color;
-                        queue.add(new Node(poll.color, nextX - step[0], nextY - step[1], count + 1));
+                        xBlue = i;
+                        yBlue = j;
+                        break;
+                    default :
                         break;
                 }
             }
         }
-        if(!isExitRed) {
-            System.out.println(-1);
-        } else {
-            if(isExitBlue) {
-                System.out.println(-1);
-            } else {
-                System.out.println(answer);
-            }
-        }
+        visited = new boolean[N][M][N][M];
+        visited[xRed][yRed][xBlue][yBlue] = true;
+        queue.add(new PositionAndCnt(xRed, yRed, xBlue, yBlue, 0));
+        bfs(queue);
+        if(answer == 11) System.out.println(-1);
         br.close();
     }
-    static class Node implements Comparable<Node> {
-        char color;
-        int x;
-        int y;
+
+    static void bfs(Queue<PositionAndCnt> queue) {
+        //first Red, last Blue
+        while (!queue.isEmpty()) {
+            PositionAndCnt poll = queue.poll();
+            int count = poll.count;
+            if (count == 10) continue;
+            for (int[] step : moving) {
+                int nextRedX = poll.xRed;
+                int nextRedY = poll.yRed;
+                int nextBlueX = poll.xBlue;
+                int nextBlueY = poll.yBlue;
+
+                //move
+                while (map[nextRedX + step[0]][nextRedY + step[1]] != '#') {
+                    nextRedX += step[0];
+                    nextRedY += step[1];
+                    if(map[nextRedX][nextRedY] == 'O') break;
+                }
+                while (map[nextBlueX + step[0]][nextBlueY + step[1]] != '#') {
+                    nextBlueX += step[0];
+                    nextBlueY += step[1];
+                    if(map[nextBlueX][nextBlueY] == 'O') break;
+                }
+
+                if(map[nextBlueX][nextBlueY] == 'O') continue;
+                if(map[nextRedX][nextRedY] == 'O') {
+                    answer = count + 1;
+                    System.out.println(answer);
+                    return;
+                }
+
+                //case that red and blue are same position.
+                if (nextRedX == nextBlueX && nextRedY == nextBlueY) {
+                    if(moveDistance(poll.xRed, poll.yRed, nextRedX, nextRedY)
+                            > moveDistance(poll.xBlue, poll.yBlue, nextBlueX, nextBlueY)) {
+                        nextRedX -= step[0];
+                        nextRedY -= step[1];
+                    } else {
+                        nextBlueX -= step[0];
+                        nextBlueY -= step[1];
+                    }
+                }
+                if(!visited[nextRedX][nextRedY][nextBlueX][nextBlueY]) {
+                    visited[nextRedX][nextRedY][nextBlueX][nextBlueY] = true;
+                    queue.add(new PositionAndCnt(nextRedX, nextRedY, nextBlueX, nextBlueY, count + 1));
+                }
+            }
+        }
+    }
+
+    private static int moveDistance(int originX, int originY, int afterX, int afterY) {
+        return Math.abs(afterX - originX) + Math.abs(afterY - originY);
+    }
+
+    static class PositionAndCnt {
+        int xRed;
+        int yRed;
+        int xBlue;
+        int yBlue;
         int count;
 
-        public Node(char color, int x, int y, int count) {
-            this.color = color;
-            this.x = x;
-            this.y = y;
+        public PositionAndCnt(int xRed, int yRed, int xBlue, int yBlue, int count) {
+            this.xRed = xRed;
+            this.yRed = yRed;
+            this.xBlue = xBlue;
+            this.yBlue = yBlue;
             this.count = count;
         }
 
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.count, o.count);
-        }
     }
 }
