f38076b2437a967a63e2b09034a41f8d4ff820c5
ospark <pos91011@gmail.com>
2024-01-01 21:07:08 +0900

    solve Baekjoon a Question using Java

---
+++ Java/code_exercies/Baekjoon/src/bronze/Welcome.java
@@ -0,0 +1,13 @@
+package bronze;
+
+/**
+ * Baekjoon 5337
+ * @link https://www.acmicpc.net/problem/5337
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
