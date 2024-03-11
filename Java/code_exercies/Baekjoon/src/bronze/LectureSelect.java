8aa05d7ed59d58b73de740eb73239d884b43b8da
ospark <pos91011@gmail.com>
2024-03-11 20:47:53 +0900

    solve Baekjoon Quesiton

---
+++ Java/code_exercies/Baekjoon/src/bronze/LectureSelect.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Baekjoon 11948
+ * @link https://www.acmicpc.net/problem/11948
+ */
+public class LectureSelect {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int A = Integer.parseInt(br.readLine());
+        final int B = Integer.parseInt(br.readLine());
+        final int C = Integer.parseInt(br.readLine());
+        final int D = Integer.parseInt(br.readLine());
+        final int E = Integer.parseInt(br.readLine());
+        final int F = Integer.parseInt(br.readLine());
+        int min = Math.min(Math.min(A, B), Math.min(C, D));
+        int firstSelect = A + B + C + D - min;
+        int secondSelect = Math.max(E, F);
+        System.out.println(firstSelect + secondSelect);
+        br.close();
+    }
+}
